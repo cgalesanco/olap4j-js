6c90ec79344912f8d2d2f200c4e54cb066e7a244
Cesar Garcia <cgalesanco@hotmail.com>
2013-03-27 07:39:38 +0000

    Added "busy" spinner

--- src/main/js/olapJsApp.js
+++ src/main/js/olapJsApp.js
@@ -5,92 +5,136 @@ require(['lib/angular', 'jquery', 'olapRowsAxis', 'olapColsAxis', 'olapService',
       var svc = new OlapService('/rest/query');
 
       angular.module('olapJsApp', []).
-        controller('QueryCtrl', ['$scope', '$http', function ($scope, $http) {
+          controller('QueryCtrl', ['$scope', '$http', function ($scope, $http) {
 
-        svc.executeQuery({success:function(data){
-          $scope.$apply(function($scope){
-            $scope.query = data;
-          });
-        }});
+            $scope.queryInProgress = true;
+            $http.get('/rest/query/hierarchies').
+                success(function (data) {
+                  $scope.hierarchies = data;
 
-        $http.get('/rest/query/hierarchies').success(function(data){
-          $scope.hierarchies = data;
-          });
+                  svc.executeQuery({success: function (data) {
+                    $scope.$apply(function ($scope) {
+                      $scope.query = data;
+                      $scope.queryInProgress = false;
+                    })
+                  }});
+                }).
+                fail(function(){
+                  $scope.queryInProgress = false;
+                });
 
-        $scope.notUsedHierarchy = function(h){
-          var axisHierarchy;
-          if ( !$scope.query ) {
-            return true;
+            $scope.notUsedHierarchy = function (h) {
+              var axisHierarchy;
+              if (!$scope.query) {
+                return true;
+              }
+
+              for (var hierarchyIdx in $scope.query.rowsAxis.hierarchies) {
+                axisHierarchy = $scope.query.rowsAxis.hierarchies[hierarchyIdx];
+                if (axisHierarchy.uniqueName === h.uniqueName) {
+                  return false;
+                }
+              }
+
+              for (hierarchyIdx in $scope.query.colsAxis.hierarchies) {
+                axisHierarchy = $scope.query.colsAxis.hierarchies[hierarchyIdx];
+                if (axisHierarchy.uniqueName === h.uniqueName) {
+                  return false;
+                }
+              }
+
+              return true;
+            }
+
+            $scope.addHierarchy = function (axis, name) {
+              $scope.queryInProgress = true;
+              svc.addHierarchy(axis, name, {
+                success: function (data) {
+                  $scope.$apply(function ($scope) {
+                    $scope.query = data;
+                    $scope.queryInProgress = false;
+                  });
+                }});
+            };
+
+            $scope.removeHierarchy = function (axis, name) {
+              $scope.queryInProgress = true;
+              svc.removeHierarchy(axis, name, {
+                success: function (data) {
+                  $scope.$apply(function ($scope) {
+                    $scope.query = data;
+                    $scope.queryInProgress = false;
+                  });
+                }});
+            };
           }
+          ]).
 
-          for(var hierarchyIdx in $scope.query.rowsAxis.hierarchies) {
-            axisHierarchy = $scope.query.rowsAxis.hierarchies[hierarchyIdx];
-            if ( axisHierarchy.uniqueName === h.uniqueName )
-              return false;
-          }
+          directive('olapCellset', function () {
+            var table;
 
-          for(var hierarchyIdx in $scope.query.colsAxis.hierarchies) {
-            axisHierarchy = $scope.query.colsAxis.hierarchies[hierarchyIdx];
-            if ( axisHierarchy.uniqueName === h.uniqueName )
-              return false;
-          }
-
-          return true;
-        }
-
-        $scope.addHierarchy = function(axis, name) {
-          svc.addHierarchy(axis, name,{
-            success:function(data){
-              $scope.$apply(function($scope){
-                $scope.query = data;
-              });
-            }});
-        }
-
-        $scope.removeHierarchy = function(axis, name) {
-          svc.removeHierarchy(axis, name,{
-            success:function(data){
-              $scope.$apply(function($scope){
-                $scope.query = data;
-              });
-            }});
-          }
-        }
-        ]).
-
-        directive('olapCellset', function(){
-          var table;
-
-          function link(scope, iElement, iAttrs) {
-            function createHandler(opName) {
-              return function (table, axis, position) {
-                svc[opName](axis,position,{
-                  success:function(data) {
-                    scope.$apply(function(scope){
-                      scope[iAttrs.olapCellset] = data;
+            function link(scope, iElement, iAttrs) {
+              function busyServiceCall() {
+                var callArgs = Array.prototype.slice.call(arguments);
+                var options = callArgs.splice(-1)[0];
+                var delayedOptions = {
+                  success: function (data) {
+                    options.success(data);
+                    scope.$apply(function (scope) {
+                      scope.queryInProgress = false;
+                      window.clearTimeout(timer);
+                    });
+                  },
+                  fail: function (data) {
+                    options.fail(data);
+                    scope.$apply(function (scope) {
+                      scope.queryInProgress = false;
+                      window.clearTimeout(timer);
                     });
                   }
-                });
-              };
+                };
+
+                var timer = window.setTimeout(function () {
+                  scope.$apply(function (scope) {
+                    scope.queryInProgress = true;
+                  });
+                }, 300);
+
+                var args = callArgs.splice(1);
+                args.push(delayedOptions);
+                arguments[0].apply(svc, args);
+              }
+
+              function createHandler(opName) {
+                return function (table, axis, position) {
+                  busyServiceCall(svc[opName], axis, position, {
+                    success: function (data) {
+                      scope.$apply(function (scope) {
+                        scope[iAttrs.olapCellset] = data;
+                      });
+                    }
+                  })
+                };
+              }
+
+              scope.$watch(iAttrs.olapCellset, function (newValue) {
+                if (newValue) {
+                  table.setData(newValue);
+                }
+              });
+
+              table.setDrillHandlers(createHandler('drill'), createHandler('undrill'));
             }
 
-            scope.$watch(iAttrs.olapCellset, function(newValue){
-              if ( newValue ) {
-                table.setData(newValue);
-              };
-            });
-
-            table.setDrillHandlers(createHandler('drill'), createHandler('undrill'));
-          }
-
-          return {
-            restrict: 'A',
-            compile: function(tElem) {
-              table = new CellSetTable(tElem, CellSetRowsAxis, CellSetColsAxis);
-              return link;
-            }
+            return {
+              restrict: 'A',
+              compile: function (tElem) {
+                table = new CellSetTable(tElem, CellSetRowsAxis, CellSetColsAxis);
+                return link;
+              }
             };
           });
+
 
       angular.bootstrap(document, ['olapJsApp']);
     });
